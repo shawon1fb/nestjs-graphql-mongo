88563978e6b873068b016642dd6da727a23bb9c4
shawon1fb <shawon0187@gmail.com>
2021-09-10 22:57:38 +0600

    mongo

---
+++ Makefile
@@ -0,0 +1,6 @@
+network:
+	docker network create grapql-mongodb
+
+
+typeorm:
+	yarn add typeorm @nestjs/typeorm mongodb @types/mongodb
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27017/nest',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
